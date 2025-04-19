eb121192ee6b8b78b48ea88627bbd1aa9fe56b86
CurtDarryl <curtdada@gmail.com>
2025-04-19 19:32:41 +0800

    status queries

---
+++ Lucky7_Inventory_System_Application/Queries/StatusQueries/GetStatusByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using static Lucky7_Inventory_System_Application.Responses.ServiceResponses;
+
+namespace Lucky7_Inventory_System_Application.Queries.StatusQueries;
+
+public class GetStatusByIdQuery : IRequest<GetResponse>
+{
+    public int Id { get; set; }
+}
