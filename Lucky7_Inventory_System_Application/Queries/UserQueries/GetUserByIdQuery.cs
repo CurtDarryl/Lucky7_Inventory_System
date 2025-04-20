b8beba0c0aacadbd21e47b4270e125ced87db6e3
CurtDarryl <curtdada@gmail.com>
2025-04-20 14:33:46 +0800

    user queries

---
+++ Lucky7_Inventory_System_Application/Queries/UserQueries/GetUserByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using static Lucky7_Inventory_System_Application.Responses.ServiceResponses;
+
+namespace Lucky7_Inventory_System_Application.Queries.UserQueries;
+
+public class GetUserByIdQuery : IRequest<GetResponse>
+{
+    public required string Id { get; set; }
+}
