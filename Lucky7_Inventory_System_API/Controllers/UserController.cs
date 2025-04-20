9697ae310dfb5f8db5f4019ec29fd436a06dec99
CurtDarryl <curtdada@gmail.com>
2025-04-20 13:06:15 +0800

    added user commands

--- Lucky7_Inventory_System_API/Controllers/UserController.cs
+++ Lucky7_Inventory_System_API/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using Lucky7_Inventory_System_Application.Commands.UserCommands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,4 +15,17 @@ public class UserController : Controller
         _mediator = mediator;
     }
 
+    [HttpPost("AddUser")]
+    public async Task<IActionResult> AddUser([FromQuery] CreateUserCommand command)
+    {
+        var response = await _mediator.Send(command);
+        return Ok(response);
+    }
+
+    [HttpPut("UpdateUser")]
+    public async Task<IActionResult> UpdateUser([FromQuery]UpdateUserCommand command)
+    {
+        var response = await _mediator.Send(command);
+        return Ok(response);
+    }
 }
---
+++ Lucky7_Inventory_System_Application/Commands/UserCommands/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using static Lucky7_Inventory_System_Application.Responses.ServiceResponses;
+
+namespace Lucky7_Inventory_System_Application.Commands.UserCommands;
+
+public class CreateUserCommand : IRequest<GetResponse>
+{
+    public string? Firstname { get; set; }  
+    public string? Lastname { get; set; }
+    public string? Password { get; set; }
+    public int RoleId { get; set; }
+    public int StatusId { get; set; }
+}
