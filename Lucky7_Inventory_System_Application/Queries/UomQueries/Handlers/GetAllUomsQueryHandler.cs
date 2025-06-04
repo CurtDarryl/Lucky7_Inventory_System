92463771f52384210149d5d5839c82379a36f2c6
LAPTOP-S5CDKRAG\Curt <curt.penote@wss.sg>
2025-06-04 13:01:20 +0800

    Uom Queries

---
+++ Lucky7_Inventory_System_Application/Queries/UomQueries/Handlers/GetAllUomsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Lucky7_Inventory_System_Application.Constants;
+using Lucky7_Inventory_System_Application.Interfaces;
+using Lucky7_Inventory_System_Domain.Entities;
+using MediatR;
+using System.Net;
+using static Lucky7_Inventory_System_Application.Responses.ServiceResponses;
+
+namespace Lucky7_Inventory_System_Application.Queries.RoleQueries.Handlers;
+
+public class GetAllUomQueryHandler : IRequestHandler<GetAllUomQuery, GetResponse>
+{
+    private readonly IGenericRepository<Uom> _repository;
+
+    public GetAllUomQueryHandler(IGenericRepository<Uom> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetResponse> Handle(GetAllUomQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var roles = await _repository.GetAll();
+            if (roles == null || !roles.Any())
+            {
+                return new GetResponse(true, null, "No Roles were Found", HttpStatusCode.NotFound);
+            }
+            return new GetResponse(true, roles, "Roles were Successfully Retrieved", HttpStatusCode.OK);
+        }
+        catch (Exception ex)
+        {
+            return new GetResponse(false, null, ex.Message, HttpStatusCode.InternalServerError);
+        }
+    }
+}
